cf378084ac08cc9172122c805ffa826096d89a0c
arthur <arthurcarvalho24@gmail.com>
2019-05-07 20:52:10 -0300

    Schema post

---
+++ models/Post.js
@@ -0,0 +1,40 @@
+const mongoose= require("mongoose")
+const Schema = mongoose.Schema;
+
+const PostSchema = new Schema({
+   image:{
+      type : Buffer,
+      required:true
+   },
+   caption:{
+      type: String  
+   },
+   allowComments:{
+      type: Boolean,
+      default: true
+   },
+   comments:[{
+      commentBody:{
+         type: String,
+         required: true
+      },
+      commentDate:{
+         type: Date,
+         default: Date.now
+      },
+      commentUser:{
+         type: Schema.Types.ObjectId,
+         ref:'users'
+      }
+   }],
+   user:{
+      type:Schema.Types.ObjectId,
+      ref:'users'
+   },
+   date:{
+      type: Date,
+      default: Date.now
+   }
+})
+
+mongoose.model("posts", PostSchema, 'posts')
