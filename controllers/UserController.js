94ac205e66c4ed4c0ca6bc8f8d1a3d01ebf7886f
arthur <arthurcarvalho24@gmail.com>
2019-05-28 16:44:39 -0300

    deletado funcao obsoleta

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -8,51 +8,36 @@ require("../models/Post")
 const Post = mongoose.model("posts")
 
 module.exports = {
-    getUserProfile: (req, res,next) => {
-        let userid = req.params.id;
-        User.findById(userid)
-           .then(user =>{
-              if(user){
-                 res.render("user/profile", {
-                    profile: user
-                 })
-              } else {
-                 req.flash("error_msg","este usuário não existe");
-                 res.redirect("/user/login")
-              }
-           })
-           .catch(err => {
-            let error = err;
-            error.httpStatusCode = 500;
-            next(error)
+   getFeed: (req, res, next) => {
+      Post.find({})
+         .populate('user')
+         .sort({
+            date: 'desc'
          })
-     },
-     getFeed:(req, res,next) => {
-        Post.find({})
-           .populate('user')
-           .sort({
-              date: 'desc'
-           })
-           .then(posts =>{
-              res.render("user/feed", {
-                 posts:posts
-              })
-           })
-           .catch(err => {
-            let error = err;
-            error.httpStatusCode = 500;
-            next(error)
-         })
-     },
-     getDashboard:(req,res,next) =>{
-        Post.find({user: req.user._id})
-         .then(posts =>{
-            res.render('user/dashboard',{posts})
+         .then(posts => {
+            res.render("user/feed", {
+               posts: posts
+            })
          })
          .catch(err => {
             let error = err;
             error.httpStatusCode = 500;
             next(error)
          })
-     }
+   },
+   getDashboard: (req, res, next) => {
+      Post.find({
+            user: req.user._id
+         })
+         .then(posts => {
+            res.render('user/dashboard', {
+               posts
+            })
+         })
+         .catch(err => {
+            let error = err;
+            error.httpStatusCode = 500;
+            next(error)
+         })
+   }
 }
